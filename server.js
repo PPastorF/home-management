ae68cbb2a71d2e348c4e1c939dc0447551d1759f
PPastorF <pedropastorf@hotmail.com>
2018-11-08 13:43:53 -0200

    Migrated to ExpressJS framework

--- server.js
+++ server.js
@@ -1,58 +1,11 @@
-const http = require('http');
-const url  = require('url');
-const fs   = require('fs');
-const port = 8080;
-    	
-// Server object
-http.createServer(function(request, response) {  
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var app = express();
+var port = 8080;
 
-	// Gets pathname of page requested by client
-	var pathname = url.parse(request.url).pathname;
-    console.log("\n");
-    console.log("Request for " + pathname + " received.");
+app.use(express.static(path.join('public')));
 
-    // Remove first '/' char from pathname, to be used as relative pathname
-    var truePath = pathname.slice(1);
-
-    // Get type of file requested
-    var requestType = truePath.slice(0, truePath.indexOf('/'));
-
-    // index.html response
- 	if (pathname == '/') {
-		truePath = 'index.html';
- 		contentType = 'text/html'
- 		requestType = 'html';
- 	}
- 	// icon response
- 	if (truePath == 'favicon.ico') {
- 		contentType = '/image/x-icon'
- 		truePath = 'img/favicon.ico'
- 	}
- 	// js response
- 	if (requestType == 'js') {
- 		contentType = 'application/javascript'
- 	}
- 	// image response
- 	if (requestType == 'img') {
- 		contentType = 'image/jpeg'
- 	}
-
-    console.log('truePath = ' + truePath);
-    console.log('requestType = ' + requestType);
-
- 	// Write http header
-	response.writeHead(200, {'Content-Type': contentType});
-	
-	// Reads file and writes it into http response
-	fs.readFile(truePath, function(err, file) {
-		if(err) {
-			throw err;
-		}
-		response.write(file);
-	    response.end();
-	});
-
-
-} ).listen(port);
-
-console.log("Listening on port " + port + "...")
+app.listen(port, function () {
+  console.log('Listening on port '+ port +'...')
+});
